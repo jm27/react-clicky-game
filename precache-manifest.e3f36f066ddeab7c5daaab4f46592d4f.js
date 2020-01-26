b5296f565d60847a25dd5fba0d8bdc7707fb38a2
Jesus Esquer <jme2791@gmail.com>
2020-01-26 16:20:58 -0700

    Updates

---
+++ precache-manifest.e3f36f066ddeab7c5daaab4f46592d4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b33db946377a8b52285a7111b6406b",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "65f4aa80e3ac21152853",
+    "url": "/react-clicky-game/static/css/main.5d067408.chunk.css"
+  },
+  {
+    "revision": "0800ed0fa1e4d9ca6495",
+    "url": "/react-clicky-game/static/js/2.46ba947a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-clicky-game/static/js/2.46ba947a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65f4aa80e3ac21152853",
+    "url": "/react-clicky-game/static/js/main.3a5bd72f.chunk.js"
+  },
+  {
+    "revision": "3e77e8ea7242182980b8",
+    "url": "/react-clicky-game/static/js/runtime-main.ad4fc7c2.js"
+  }
+]);
